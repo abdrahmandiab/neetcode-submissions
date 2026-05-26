6b7632d214e7213aa32e44f30164020aa8bfe867
Abdelrahman Diab <37490334+abdrahmandiab@users.noreply.github.com>
2026-05-26 13:09:00 +0200

    Add: k-closest-points-to-origin - submission-8

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-8.py
@@ -0,0 +1,14 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        kClos, heap = [], []
+        heapq.heapify(heap)
+
+        for p in points:
+            heapq.heappush(heap, (self.euclid(p),p))
+        print( heap)
+        for i in range(k):
+            dist, p = heapq.heappop(heap)
+            kClos.append(p)
+        return kClos
+    def euclid(self,p) -> float:
+        return math.sqrt(math.pow(p[0],2) + math.pow(p[1],2))
