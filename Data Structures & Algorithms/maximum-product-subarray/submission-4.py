b6d7a4a46e9226316b0817f14df5d4b8dd42a07a
Abdelrahman Diab <37490334+abdrahmandiab@users.noreply.github.com>
2026-07-01 18:16:36 +0200

    Add: maximum-product-subarray - submission-4

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-4.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        n = len(nums)
+        
+        result = cur_max = cur_min = nums[0]
+        for x in nums[1:]:
+            candidates = (x, cur_max *x, cur_min *x)
+            cur_max = max(candidates)
+            cur_min = min(candidates)
+            result = max(result, cur_max)
+        return result
+
+# What is the smallest subproblem I am solving?
+
+
+# Since we are dealing with product, and -ve * -ve = positive, 
+# We could need to consider a negative product up to now and a positive one
+#  butttt actually we don't -> since the subarray is contiguous
+# so the negative thing just tells us, we shouldn't simply break on negative.
+# i.e. for [2,4,-3,5] largest is [2,4] -> 2*4 = 8. if we take the -3, it becomes -24
+#    and for [-3,1,-2] largest is [-3,1,-2] -> -3*1*-2 = 6. Two negatives made a positive.
+# we need to calculate each subarray in the fashion:
+#   for i in range(n):
+#       for j in range(i+1,n):
+#           check(nums[i:j])
+# But, this leads to recomputing the same subcomponents of nums[i:j]
+#   i.e. ffor nums[0:5], we use the calculation of nums[0:4]*nums[5] and for or nums[0:6] we use nums[0:5]*nums[6]
