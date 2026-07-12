c0534ed9084e57a629972f70a0720912d7cf89a9
Abdelrahman Diab <37490334+abdrahmandiab@users.noreply.github.com>
2026-07-12 21:58:41 +0200

    Add: valid-tree - submission-5

---
+++ Data Structures & Algorithms/valid-tree/submission-5.py
@@ -0,0 +1,55 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n+1))
+        self.size = [1]*(n+1)
+    def find(self, u):
+        if self.parent[u] != u:
+            self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+
+    def union(self, u,v):
+        pu = self.find(u)
+        pv = self.find(v)
+        if pu == pv:
+            return False
+
+        if self.size[pu] >= self.size[pv]:
+            self.size[pu] += self.size[pv]
+            self.parent[pv] = pu
+        else:
+            self.size[pv] += self.size[pu]
+            self.parent[pu] = pv
+        return True
+
+
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges)!=n-1:
+            return False
+        dsu = DSU(n)
+ 
+        for fromm,to in edges:
+            if not dsu.union(fromm, to):
+                return False
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
